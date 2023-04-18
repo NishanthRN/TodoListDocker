cac0776022170b772669b9a7b90b0f53ef43267c
NishanthRN <nishanth13.rajesh@gmail.com>
2023-04-19 02:25:15 +0530

    first commit

--- server/models/db.js
+++ server/models/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
-
-module.exports = mongoose.connect('mongodb://localhost:27017/todolist',{
-    useUnifiedTopology : true,
-    useNewUrlParser : true,
-    useFindAndModify : false
+// mongodb://localhost:27017/todolist
+module.exports = mongoose.connect('mongodb+srv://nishanthrn:nishanthrn@cluster0.slwef4a.mongodb.net/todolist?retryWrites=true&w=majority', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
 }, err => {
-    if(err) console.log(`Error in DB Connection ${err}`)
+    if (err) console.log(`Error in DB Connection ${err}`)
     console.log(`MongoDB Connection Suceeded...`)
 })
